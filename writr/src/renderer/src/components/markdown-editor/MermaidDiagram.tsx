0180330f2a23c544b3a83c494f902fd48009dfc8
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-09-08 22:26:12 +0600

    Mermaid update

--- writr/src/renderer/src/components/markdown-editor/MermaidDiagram.tsx
+++ writr/src/renderer/src/components/markdown-editor/MermaidDiagram.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useState, useMemo } from 'react'
+import { useEffect, useState } from 'react'
 
 let mermaidInstance: typeof import('mermaid').default | null = null
 const getMermaid = async () => {
